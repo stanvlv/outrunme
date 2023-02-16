b78183aec219bda84710055c058f08fb8f47421f
Ana Rosa <asofiamcarvalho@gmail.com>
2023-02-16 10:09:22 +0100

    Added login and sign up screens V1

--- App.jsx
+++ App.jsx
@@ -4,10 +4,11 @@
  *
  * @format
  */
-import LoginSignup from './src/screens/LoginSignup';
+
 import {MapView} from 'react-native-maps';
 import {Button} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NativeBaseProvider, Box} from 'native-base';
 import React, {useState, useEffect} from 'react';
 import {PropsWithChildren} from 'react';
@@ -30,15 +31,19 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-
-
-
+import {useNavigation} from '@react-navigation/native';
+import SignUp from './src/screens/SignUp';
+import Login from './src/screens/Login';
 export default function App() {
-  return <NavigationContainer>
-    <LoginSignup />
-  </NavigationContainer>;
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
--- src/screens/LoginSignup.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, NativeBaseProvider } from "native-base"
-
-export default function LoginSignup() {
-    return (
-        <NativeBaseProvider>
-        <Box>Hello</Box>
-        </NativeBaseProvider>
-    )
-}
